50e4d65300ddba666241b3c780d50fe4304bad47
sandilem7789 <sandilem7789@gmail.com>
2021-06-02 08:00:54 +0200

    ds and a

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
+/*
+**Here I am learning Data Structures and Algorithms
+**/
+
+//Comparing floating numbers is not straight forward in javascript
+console.log("\n");
+console.log("****************Numbers******************");
+console.log(`0.1 + 0.2 === 0.3: ${0.1 + 0.2 === 0.3} `);
+
+//Number.EPSILON returns the smallest interval between twoo representable numbers.
+//this is useful for the problem with floating-point approximation
 let numberEquals = (x, y) => {
     return Math.abs(x - y) < Number.EPSILON;
 }
 
 let sum = numberEquals(0.1 + 0.2, 0.3);
 
-console.log(sum);
+console.log(`But with Number.EPSILON, 0.1 + 0.2 === 0.3: ${sum}`);
+console.log("___________________________________________________");
+console.log("\n");
+console.log("****************Numbers: Maximums***************");
+
+let max = Number.MAX_SAFE_INTEGER + 1 === Number.MAX_SAFE_INTEGER + 2;
+console.log(`Number.MAX_SAFE_INTEGER + 1 === Number.MAX_SAFE_INTEGER + 2 returns: -${max}- because javascript cant go any higher than this!`);
